1e2fbecc2b3532de1f27c588ee2b84d73ca65941
Matei Popescu <popescumateicalin1510@gmail.com>
2024-07-03 20:15:07 +0300

    reorganized files

---
+++ src/positional_features.py
@@ -0,0 +1,28 @@
+import chess
+
+
+def game_phase(board):
+    return None
+
+
+def king_safety_score(board, color):
+    return None
+
+
+def pawn_structure(board, color):
+    return None
+
+
+def piece_activity(board, color):
+    return None
+
+
+def center_control_score(board, color):
+    return None
+
+
+def open_file_control(board, color):
+    return None
+
+
+## etc
