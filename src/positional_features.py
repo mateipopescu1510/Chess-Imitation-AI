5d208f67060aba0cf0ea244bb65631a3170fb972
Matei Popescu <popescumateicalin1510@gmail.com>
2024-07-05 17:55:21 +0300

    added stockfish eval to every position in dataset

--- src/positional_features.py
+++ src/positional_features.py
@@ -1,8 +1,35 @@
 import chess
 
 
-def game_phase(board):
-    return None
+def material(board, color):
+    piece_values = {
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9,
+    }
+    material = 0
+
+    for piece in piece_values:
+        material += len(board.pieces(piece, color)) * piece_values[piece]
+
+    return material
+
+
+def game_phase(fen):
+    board = chess.Board(fen)
+
+    if board.fullmove_number < 16:
+        return "opening"
+
+    white_material = material(board, chess.WHITE)
+    black_material = material(board, chess.BLACK)
+
+    if white_material < 16 and black_material < 16:
+        return "endgame"
+    else:
+        return "middlegame"
 
 
 def king_safety_score(board, color):
